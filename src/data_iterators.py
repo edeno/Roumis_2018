4f68a9bf53484b9ebbde6c176e84b3edbbf448cd
droumis <roumis.d@gmail.com>
2018-02-25 17:14:55 -0800

    fixing and generalizing time chunk iterator

--- src/data_iterators.py
+++ src/data_iterators.py
@@ -2,74 +2,91 @@ import numpy as np
 import pandas as pd
 
 
-def create_segments_marks_df(marks_df, ntrodes, segments_times, position,
-                             resample_time='1ms'):
+def chunk_mark_timeseries(timeseries_df, ntrodes, segments_times, position=0,
+                          resample_time='1ms'):
     '''With row per segment
 
     Parameters
     ----------
-    marks_df : pandas.DataFrame
-    ntrodes : int??
-    segment_times : pandas.DataFrame
+    timeseries_df : list of pandas.DataFrame's (one per source, i.e. ntrode; each df requires common time index, NaN empty time rows)
+    ntrodes : list of ntrodes to include
+    segments_times : pandas.DataFrame
     position : pandas.DataFrame
     resample_time : str
 
     Returns
     -------
     segments_marks : pandas.DataFrame
-
+    COLUMN_NAMES = ['segment_ID', 'segment_type', 'area', 'start_time',
+                    'end_time', 'marks', 'ntrodes', 'linear_position', 'times']
     '''
     COLUMN_NAMES = ['segment_ID', 'segment_type', 'area', 'start_time',
-                    'end_time', 'marks', 'ntrodes', 'linear_position']
+                    'end_time', 'marks', 'ntrodes', 'linear_position', 'times']
     segments_marks = pd.DataFrame(columns=COLUMN_NAMES)
 
-    segments_marks['marks'] = [np.stack(
-        [(marks_df[ntrode - 1][seg['start_time']:seg['end_time']]
-          .resample(resample_time).mean().index)
-         for ntrode in ntrodes]
-        for segind, seg in segments_times.iterrows(), axis=0)]
+    segments_marks['marks'] = [
+        pd.concat([
+            (timeseries_df[introde][seg['start_time']:seg['end_time']]
+             .resample(resample_time).mean(axis=0))
+            for introde, ntrode in enumerate(ntrodes)], axis=1)
+        for segind, seg in segments_times.iterrows()
+    ]
+    if position:
+        segments_marks['linear_position'] = chunk_position_timeseries(
+            position, resample_time, event_times)
 
-    segments_marks['time'] = [
-        marks_df[0][seg['start_time']:seg['end_time']].resample(
-            resample_time).mean().index
-        for segind, seg in segments_times.iterrows()]
-    # bin times by period resample_time in ms "500ms" (via resample)
-
-    segments_marks['segment_ID'] = segments_marks.index
-    segments_marks['start_time'] = segments_times['start_time']
-    segments_marks['end_time'] = segments_times['end_time']
-    segments_marks['linear_position'] = [
-        position[seg['start_time']:seg['end_time']
-                 ].resample(resample_time).mean().index
-        for segind, seg in segments_times.iterrows()]
-
+    segments_marks['segment_ID'] = event_times.index
+    segments_marks['start_time'] = event_times['start_time']
+    segments_marks['end_time'] = event_times['end_time']
     return segments_marks
 
+def chunk_position_timeseries(position, resample_time, event_times):
+    position_chunks = [
+        position[seg['start_time']:seg['end_time']
+                 ].resample(resample_time).mean(axis=0)
+        for segind, seg in segments_times.iterrows()
+        ]
+    return position_chunks
 
-def chunk_timeseries(num_of_chunks, timeseries_df):
-    '''linspaced chunks of timeseries
+def linspaced_epoch_times(num_of_chunks, timeseries_df, animals, epoch_key):
+    from loren_frank_data_processing import get_trial_time
+    '''linspaced times to chunk timeseries
 
     Parameters
     ----------
     num_of_chunks : int (the number of chunks)
     timeseries_df : pandas.DataFrame. (requires time index)
+    epoch_key : tuple e.g. (str(animal), int(day), int(epoch))
 
     Returns
     -------
     segment_times : pandas.DataFrame (start_time, end_time columns)
 
     '''
-    startstamp = timeseries_df[0].index[0].value  # get epoch start and end
-    endstamp = timeseries_df[0].index[-1].value
+
+    epoch_time = get_trial_time(epoch_key, animals)
+    startstamp = epoch_time[0].total_seconds()
+    endstamp = epoch_time[-1].total_seconds()
     segments_times = pd.DataFrame(columns=['start_time', 'end_time'])
     segtimes = np.linspace(startstamp, endstamp,
-                           num_of_chunks + 2, endpoint=True)
-    segments_times['start_time'] = pd.TimedeltaIndex(segtimes[0:-2])
-    segments_times['end_time'] = pd.TimedeltaIndex(segtimes[1:-1])
+    num_of_chunks + 2, endpoint=True)
+    segments_times['start_time'] = segtimes[0:-2]
+    segments_times['end_time'] = segtimes[1:-1]
     return segments_times
 
 
-def create_segments_times_fromEvents(event_times):
-    '''Create times df from events'''
-    event_segments_times = event_times
-    return event_segments_times
+def get_event_times(event_times):
+    '''peri-event timeseries chunks
+    currently this is just a dummy handle
+
+    Parameters
+    ----------
+    event_times : pandas.DataFrame. (start_time, end_time columns)
+
+    Returns
+    -------
+    segments_times : pandas.DataFrame. (start_time, end_time columns)
+
+    '''
+    segments_times = event_times
+    return segments_times
