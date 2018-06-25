c5dd92da4526781b2c2a2e8b1a49b2a429f915a6
Eric Denovellis <edeno@bu.edu>
2018-06-25 16:57:43 -0400

    Only analyze wtrack

--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -51,8 +51,7 @@ def main():
     args = get_command_line_arguments()
     if args.Animal is None and args.Day is None and args.Epoch is None:
         epoch_info = make_epochs_dataframe(ANIMALS)
-        epoch_keys = epoch_info[(epoch_info.type == 'run') & (
-            epoch_info.environment != 'lin')].index
+        epoch_keys = epoch_info[epoch_info.environment == 'wtrack'].index
     else:
         epoch_keys = [(args.Animal, args.Day, args.Epoch)]
 
